b5f2afbd871b2d2a78b259233a22132087dbbd4d
Константин Власов <knvlasov@reg.ru>
2021-12-23 00:12:41 +0300

    Добавил наброски разбора лексем по типам и работу со стеком

--- calc.lisp
+++ calc.lisp
@@ -7,7 +7,7 @@
             (do ((line (read s nil 'eof)
                        (read s nil 'eof)))
                 ((eql line 'eof))
-                (push line r))
+                (push line r)) 
             (reverse r)
         )
     )
@@ -20,6 +20,23 @@
     (terpri)
     (write (expression-to-lexemes-list expression))
     (terpri)
+    (let ((stack nil))
+        (loop for lexeme in (expression-to-lexemes-list expression) do 
+            (terpri)
+            (cond 
+                ((numberp lexeme) 
+                    (write 'num)
+                    (push lexeme stack)
+                )
+                ((symbolp lexeme)
+                    (write 'sym)
+                )
+            )
+        )
+        (write stack)
+    )
+
+
 
 )
 
