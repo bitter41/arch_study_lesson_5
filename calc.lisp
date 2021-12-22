915a8a4bc839185bcc17801fed0bdfaaa3a369d5
Константин Власов <knvlasov@reg.ru>
2021-12-22 23:04:24 +0300

    Написал разбор выражения на лексемы

--- calc.lisp
+++ calc.lisp
@@ -1,5 +1,26 @@
 
-(defun hello ()
-           (format t "Hello, World!~%"))
-(hello)
+(defvar expression)
 
+(defun expression-to-lexemes-list(expression)
+    (with-input-from-string (s expression)
+        (let ((r nil))
+            (do ((line (read s nil 'eof)
+                       (read s nil 'eof)))
+                ((eql line 'eof))
+                (push line r))
+            (reverse r)
+        )
+    )
+)
+
+(defun calc-reverse-polish-expression()
+    (princ "Enter expression: ")
+    (terpri)
+    (setq expression (read-line))
+    (terpri)
+    (write (expression-to-lexemes-list expression))
+    (terpri)
+
+)
+
+(calc-reverse-polish-expression)
